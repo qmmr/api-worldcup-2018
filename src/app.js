42b51a75a4749fb104d0fe75aea821a307aeaa7c
Marcin Kumorek <kumeek@gmail.com>
2018-07-08 18:01:43 +0200

    Connect to mlab mongodb server

--- src/app.js
+++ src/app.js
@@ -1,12 +1,23 @@
+import dotenv from 'dotenv'
 import express from 'express'
 import graphqlHTTP from 'express-graphql'
 import mongoose from 'mongoose'
 import schema from './schema'
 
+dotenv.config()
+
+// TODO: Check env variables and print error when not provided
+const DB_USER = process.env.DB_USER || 'admin'
+const DB_PASSWORD = process.env.DB_PASSWORD || 'password'
+const DB_HOST = process.env.DB_HOST || '127.0.0.1'
+const DB_PORT = process.env.DB_PORT || '27017'
+const DB_NAME = process.env.DB_NAME || 'fifa-worldcup-2018'
+
 const app = express()
 const PORT = 3000
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost/world-cup-2018')
+
+mongoose.connect(`mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`)
 
 app.use('/graphql', graphqlHTTP({ graphiql: true, schema }))
 
