9929259318349624f5baf5a3af62c4659bc38e70
Marcin Kumorek <kumeek@gmail.com>
2018-07-17 09:41:02 +0200

    Switch to mlab.com as DB

--- src/app.js
+++ src/app.js
@@ -4,20 +4,39 @@ import graphqlHTTP from 'express-graphql'
 import mongoose from 'mongoose'
 import schema from './schema'
 
+mongoose.Promise = global.Promise
 dotenv.config()
 
-// TODO: Check env variables and print error when not provided
 const DB_USER = process.env.DB_USER || 'admin'
 const DB_PASSWORD = process.env.DB_PASSWORD || 'password'
 const DB_HOST = process.env.DB_HOST || '127.0.0.1'
 const DB_PORT = process.env.DB_PORT || '27017'
 const DB_NAME = process.env.DB_NAME || 'fifa-worldcup-2018'
+const URL = process.env.URL || 'https://www.fifa.com/worldcup/matches/#groupphase'
+
+export const connect = async () => {
+  try {
+    const db = await mongoose.connect(`mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`)
+    console.log(
+      `Connection as user "${DB_USER}" to mongodb://${DB_HOST}:${DB_PORT}/${DB_NAME}\n has been established! 🎉\n`
+    )
+
+    return {
+      db,
+      disconnect: () => {
+        console.info('\nClosing connection to the DB\nBye! 👋\n')
+        db.close()
+      },
+    }
+  } catch (error) {
+    throw error
+  }
+}
 
 const app = express()
 const PORT = 3000
-mongoose.Promise = global.Promise
 
-mongoose.connect(`mongodb://${DB_USER}:${DB_PASSWORD}@${DB_HOST}:${DB_PORT}/${DB_NAME}`)
+const { db, disconnect } = connect()
 
 app.use('/graphql', graphqlHTTP({ graphiql: true, schema }))
 
